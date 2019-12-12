0524174a0d124f31b4b1449b95e9f9dddc89abbd
Julio Bueso <julio.bueso@allasrepuestos.com>
2019-12-12 07:26:07 -0600

    a

--- index.php
+++ index.php
@@ -63,7 +63,7 @@
     <div class="row text-center">
 
       <div class="col-lg-3 col-md-6 mb-4">
-        <div class="card h-100">
+        <div class="card">
           <img class="card-img-top" src="img/guia1.png" alt="">
           <!-- <div class="card-body">
             <h4 class="card-title">Card title</h4>

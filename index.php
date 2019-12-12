61ef594b2b280e7dfae77b8bb8ddce9e5ab95c3d
Julio Bueso <julio.bueso@allasrepuestos.com>
2019-12-12 07:16:44 -0600

    portada

--- index.php
+++ index.php
@@ -53,7 +53,9 @@
     <!-- Jumbotron Header -->
     <header class="jumbotron my-4">
       <h1 class="display-3">Roberto Antonio Castejón Benites</h1>
-      <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.</p>
+      <p class="lead">Lic. Ricardo Lagos.</p>
+      <p class="lead">Materia: Computación 2.</p>
+      <p class="lead">Villanueva, Cortes 2019</p>
       <a href="#" class="btn btn-primary btn-lg">Call to action!</a>
     </header>
 

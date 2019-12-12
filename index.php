7bb3f3419d5695f23f8104abdb65897e581413ea
Julio Bueso <julio.bueso@allasrepuestos.com>
2019-12-12 07:17:56 -0600

    a

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
             <a class="nav-link" href="#">Services</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Contact</a>
+            <a class="nav-link" href="#">Contacto</a>
           </li>
         </ul>
       </div>

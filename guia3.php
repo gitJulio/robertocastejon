784c863867adc96adfc97d1487d04673c54eb7af
Julio Bueso <julio.bueso@allasrepuestos.com>
2019-12-12 10:18:55 -0600

    a

--- guia3.php
+++ guia3.php
@@ -40,7 +40,7 @@
 
 
           <center>
-          <h1>GUIA No. 2</h1>
+          <h1>GUIA No. 3</h1>
           </center>
                
 
@@ -49,42 +49,35 @@
         <div class="container">
             <div class="row justify-content-md-center">
                 <div class="col col-lg-12">   
-                    <h5>1. ¿Qué es un sistema de información?<h5>
+                    <h5>1. ¿Qué es HTML<h5>
                 </div>
                 <div class="col col-lg-12">
                 <p>
-                R// Es un conjunto de partes o elementos que interactúan con un objetivo común, pero de estos los que nos interesan son los sistemas.
+                R// Es un lenguaje con el que se define el contenido de las páginas web, básicamente se trata de un
+                 conjunto de etiquetas que sirven para definir el texto y otros elementos, como imágenes, listas videos.
                 </p>
                 </div>
             </div>
 
             <div class="row justify-content-md-center">
                 <div class="col col-lg-12">   
-                    <h5>2. Enumere y explique cada uno de los tipos de sistemas de información<h5>
+                    <h5>2. ¿Qué una página web?<h5>
                 </div>
                 <div class="col col-lg-12">
                 <p>
-                R// Son:
+                R// Es un documento o información electrónica capaz de contener textos, sonidos, 
+                videos, programas, enlaces, imágenes, y muchas otras cosas, adaptadas para la llamada Word wide (w.w.w).
                 </p>
-                <ul>
-                    <li>Sistema de procesamiento de transacciones</li>
-                    <li>Sistema de control de procesos de negocio</li>
-                    <li>Sistema de colaboración empresarial</li>
-                    <li>Sistema de información de gestió</li>
-                    <li>Sistema de apoyo a la toma de decisiones</li>
-                    <li>Sistema de información ejecutiv</li>
-                </ul>
                 </div>
             </div>
 
             <div class="row justify-content-md-center">
                 <div class="col col-lg-12">   
-                    <h5>3. ¿Cuál es el ciclo de vida de un sistema de información?<h5>
+                    <h5>3. Describa los tipos de páginas web existente<h5>
                 </div>
                 <div class="col col-lg-12">
                 <p>
-                R// es un conjunto de actividades que los analistas, diseñadores, desarrolladores, y usuarios realizan para desarrollar e implementar un sistema de 
-                información.
+                R// Tipos pdf, doc. ppt, xls, txt
                 </p>
                 
                 </div>
@@ -92,21 +85,12 @@
 
             <div class="row justify-content-md-center">
                 <div class="col col-lg-12">   
-                    <h5>4. Enumere las ventajas que aportan a una empresa los sistemas de información.<h5>
+                    <h5>4. ¿Cuál es la función del JavaScript?<h5>
                 </div>
                 <div class="col col-lg-12">
                 <p>
-                 R// Ventaja:
+                R//Son las soluciones de todos los problemas tanto en el JavaScript como el resto de3l lenguaje de programación.
                 </p>
-                <ul>
-                    <li>Correo electrónico personal o empresarial</li>
-                    <li>Comercio electrónico</li>
-                    <li>Chat en tiempo real</li>
-                    <li>Video llamada</li>
-                    <li>Transacciones bancarias</li>
-                    <li>Juegos y películas en líneas</li>
-                    <li>La biblioteca digital más grande del mundo</li>
-                </ul>
                 </div>
             </div>
 
@@ -116,42 +100,48 @@
                 </div>
                 <div class="col col-lg-12">
                 <p>
-                R// El hardware, equipo físico utilizado para procesar y almacenar datos, el hardware y los procesamientos utilizados para transformar y 
-                extraer información.
+                R//Es un lenguaje que describe la presentación de los documentos estructurados, en hojas de estilo para diferentes métodos de interpretación.
                 </p>
                 </div>
             </div>
 
             <div class="row justify-content-md-center">
                 <div class="col col-lg-12">   
-                    <h5>6. ¿Cómo influye el ser humano el uso de sistemas de información?<h5>
+                    <h5>6. ¿Cuáles son los sistemas de gestión de contenidos? Enumere algunos de ellos<h5>
                 </div>
                 <div class="col col-lg-12">
                 <p>
-                R// Influye en el conocimiento en el uso de las computadoras.
+                R//Es un programa de informáticos que permite crear un entorno de trabajo para la creación y administración de contenidos 
+                principalmente en páginas web, por partes de los administradores, editores participantes y usuarios.
                 </p>
                 </div>
             </div>
                         <!--aaaaaaaaaaaaaaaaaaaaaaaaaaa-->
             <div class="row justify-content-md-center">
                 <div class="col col-lg-12">   
-                    <h5>7. ¿Qué es piratería?<h5>
+                    <h5>7. Enumere las funciones de los CMS<h5>
                 </div>
                 <div class="col col-lg-12">
                 <p>
-                R// Es algo ilícito, fuera de la ley
+                R//son:
                 </p>
+                <ul>
+                    <li>Creación de contenidos</li>
+                    <li>Gestión de contenidos</li>
+                    <li>Publicación de contenidos</li>
+                    <li>Presentación de contenidos</li>
+                </ul>
                 </div>
             </div>
 
             <div class="row justify-content-md-center">
                 <div class="col col-lg-12">   
-                    <h5>8. ¿Qué la ética computacional?<h5>
+                    <h5>8. ¿Qué la diagramación de páginas web?<h5>
                 </div>
                 <div class="col col-lg-12">
                 <p>
-                R// Es la transformadora de tres niveles distintos: Intervienen en el mundo modificando, sirve de elemento mediador en nuestras intervenciones 
-                en el modificador nuestras capacidades de acción y en capaz de crear novedosas estructuras de intervención.
+                R// Es un oficio de diseño editorial que se encarga de organizar en un espacio, contenidos escritos, visuales y en algunos casos audiovisuales 
+                (multimedios) en medios impresos y electrónicos como libros, diarios y revistas.
                 </p>
                 </div>
             </div>
@@ -159,60 +149,99 @@
             
             <div class="row justify-content-md-center">
                 <div class="col col-lg-12">   
-                    <h5>9. ¿Qué es la piratería?<h5>
+                    <h5>9. ¿Qué es un servidor web?<h5>
                 </div>
                 <div class="col col-lg-12">
                 <p>
-                R// Es algo ilícito fue de la ley
+                R//es un programa informático, que procesa una aplicación del lado del servidor.
                 </p>
                 </div>
             </div>
 
             <div class="row justify-content-md-center">
                 <div class="col col-lg-12">   
-                    <h5> 10. ¿Cómo afecta a las empresas la violación de los derechos del autor?<h5>
+                    <h5> 10. ¿Qué protocolo se usan para hacer consultas web?<h5>
                 </div>
                 <div class="col col-lg-12">
                 <p>
-                R//Afecta obras de distintos tipos, como la música, la literatura etc.
-                </p>
-                </div>
-            </div>
-
-            <div class="row justify-content-md-center">
-                <div class="col col-lg-12">   
-                    <h5>11. Enumere algunas responsabilidades que adquieren los profesionales información.<h5>
-                </div>
-                <div class="col col-lg-12">
-                <p>
-                R// Son:
+                R//son:
                 </p>
                 <ul>
-                    <li>Contar con un juicio sereno ético y moral</li>
-                    <li>Atacar y hacer cumplir las normas morales y ética</li>
-                    <li>Ser imparcial en los juicios que imite como auditor</li>
+                    <li>ARP (Address Resolution PROTOCOL), utilizado para resolución de direcciones.</li>
+                    <li>FTP (File Transfer PROTOCOL), utilizado para la transferencia de archivos</li>
                 </ul>
                 </div>
             </div>
 
             <div class="row justify-content-md-center">
                 <div class="col col-lg-12">   
-                    <h5>12. ¿De qué se encarga la seguridad informática?<h5>
+                    <h5>11. ¿Qué es alojamiento web?<h5>
                 </div>
                 <div class="col col-lg-12">
                 <p>
-                R// De proteger la integridad y la privacidad de la información almacenada en un sistema informático.
+                R// Es un servicio que provee a los usuarios de internet un sistema para poder almacenar información, imágenes, videos, o cualquier contenido accesible vía web.
                 </p>
                 </div>
             </div>
 
             <div class="row justify-content-md-center">
                 <div class="col col-lg-12">   
-                    <h5>13. ¿Cuál es la mejor manera de estar protegido al usar internet?<h5>
+                    <h5>12. Enumere algunos de los principales web hosting.<h5>
                 </div>
                 <div class="col col-lg-12">
                 <p>
-                R//Por en medio de un antivirus.
+                R// Son.
+                </p>
+                <ul>
+                    <li>Whisper</li>
+                    <li>Vimeo</li>
+                    <li>Evernote</li>
+                </ul>
+                </div>
+            </div>
+
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5>13. . ¿De qué está compuesto un dominio?<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                R// Es el nombre que identifica un sitio web, cada dominio tiene qué único en internet ejemplo: www.masadelant.com
+                </p>
+                </div>
+            </div>
+
+            
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5>13. ¿De qué está compuesto un dominio?<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                R// Es el nombre que identifica un sitio web, cada dominio tiene qué único en internet ejemplo: www.masadelant.com
+                </p>
+                </div>
+            </div>
+
+            
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5>14. Detalle las ventajas de un hosting de paga contra uno gratuito.<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                R// Solamente que es gratuito
+                </p>
+                </div>
+            </div>
+
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5>14. ¿Cómo se conoce generalmente a la configuración de donde se accede a un servicio de hosting?<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                R// se le conoce como una cuenta de correo de base de datos.
                 </p>
                 </div>
             </div>

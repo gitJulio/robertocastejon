340ff4c6c3f4e06843cd89f59b2bc06289c818c9
Julio Bueso <julio.bueso@allasrepuestos.com>
2019-12-12 10:56:35 -0600

    a

---
+++ guia6.php
@@ -0,0 +1,182 @@
+<html>
+    <head>
+        <title>
+            Guia 6
+        </title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+<link href="css/heroic-features.css" rel="stylesheet">
+    </head>
+    <body>
+          <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">UCENM</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php">Inicio
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <!-- <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contacto</a>
+          </li> -->
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+        <br>
+
+        <center>
+            <h1>GUIA No. 6</h1>
+        </center>
+
+        
+        <div class="container">
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5>1. ¿Qué es un proyecto, y cuáles son sus elementos?<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                R// Es un esfuerzo temporal que se emprende con el objetivo de crear un producto o servicio único.
+                </p>
+                <ul>
+                    <li>
+                    Principales elementos:
+                    <ol>
+                        <li>Plantificación estratégica</li>
+                        <li>Desarrollo de producto</li>
+                        <li>Comunicación</li>
+                        <li>Recurso</li>
+                    </ol>
+                    </li>
+                </ul>
+                </div>
+            </div>
+
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5>2. Detalle las fases de un proyecto.<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                R// Son:
+                </p>
+                <ul>
+                    <li>Inicio</li>
+                    <li>Planificación</li>
+                    <li>Ejecución y monitorización</li>
+                    <li>Cierre de proyecto</li>
+                </ul>
+                </div>
+            </div>
+
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5>3. ¿Qué es MS Proyect y en que ayuda en la ejecución de proyectos?<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                R// Es un software de administración de proyectos diseñados desarrollado y comercializado para 
+                asistir a administradores de proyectos en el desarrollo de planes y asignación de recursos a tareas.
+                </p>
+                </div>
+            </div>
+
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5>4. ¿Cuáles son las ventajas que ofrece un cronograma de actividades?<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                R// Son.
+                </p>
+                <ul>
+                    <li>Minimizar los riesgos y las posibilidades de fracaso del proyecto</li>
+                    <li>Permitir visualizar de manera global las diferentes etapas y actividades que componen el proyecto.</li>
+                    <li>Favorece la consecución de3 objetivos concretos.</li>
+                    <li>Potencia la comunicación entre todas las personas implicadas en el proyecto.</li>
+                    <li>Facilita la evaluación de resultados y la toma de decisiones.</li>
+                </ul>
+                </div>
+            </div>
+
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5>5. ¿Qué es un diagrama de Gantt?<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                R//Es una herramienta que se emplea para planificar y programar tareas a lo largo de un periodo determinado de tiempo.
+                </p>
+                </div>
+            </div>
+
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5> 6. ¿Qué son las actividades críticas y la ruta crítica?<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                R// Son aquellas que al retrasarse afectan al tiempo de finalización de un proyecto, y aunque todas las tareas son importantes por lo general solo algunas de ellas son críticas.
+                </p>
+                </div>
+            </div>
+                        <!--aaaaaaaaaaaaaaaaaaaaaaaaaaa-->
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5>7. Detalle que tipos de recursos se pueden usar en MS Proyecto.<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                R// Son:
+                </p>
+                <ul>
+                    <li>Recurso de trabajo</li>
+                    <li>Recurso material</li>
+                    <li>Recurso de costo</li>
+
+                </ul>
+                </div>
+            </div>
+
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5>8. ¿Por qué es importante hacer una ruta crítica?<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                R// Porque la ruta crítica no es fija y puede cambiar si alguna tarea se retrasa de 5 a 6 días entonces se convierte en ruta crítica.
+                </p>
+                </div>
+            </div>
+
+            
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5>9. ¿Qué es un diagrama de red?<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                R// Es una forma gráfica de ver tareas, dependencia y las rutas criticas del proyecto.
+                </p>
+                </div>
+            </div>
+
+        </div>        
+
+    </body>
+</html>
+

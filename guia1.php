c0db34dde21155292e0756c7d79274e12603dfb1
Julio Bueso <julio.bueso@allasrepuestos.com>
2019-12-12 09:07:56 -0600

    a

--- guia1.php
+++ guia1.php
@@ -8,9 +8,7 @@
 <link href="css/heroic-features.css" rel="stylesheet">
     </head>
     <body>
-        <center>
-        
-        </center>
+
         <br>
 
         <div class="container">
@@ -18,17 +16,9 @@
                 <div class="col col-lg-6">   
                 <h1>GUIA No. 1</h1>
                 </div>
-                <div class="col col-lg-6">
-                <div class="col-lg-3 col-md-6 mb-4">
-                    <div class="card">
-                    <img class="card-img-top" src="img/guia2.png" alt="">
-                    <!-- <div class="card-body">
-                        <h4 class="card-title">Card title</h4>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
-                    </div> -->
-                    <div class="card-footer">
-                        <a href="#" class="btn btn-primary">Ir Inicio</a>
-                    </div>
+                <div class="col col-lg-2">
+                    <div class="col-lg-3 col-md-6 mb-4">
+                            <a href="#" class="btn btn-primary">Ir Inicio</a>
                     </div>
                 </div>
                 </div>

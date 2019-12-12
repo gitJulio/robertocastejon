171447876561772aad42a6d765a2ed8b56e4685b
Julio Bueso <julio.bueso@allasrepuestos.com>
2019-12-12 10:09:38 -0600

    a

--- guia2.php
+++ guia2.php
@@ -49,115 +49,109 @@
         <div class="container">
             <div class="row justify-content-md-center">
                 <div class="col col-lg-12">   
-                    <h5>1. ¿Explique la evolución del internet?<h5>
+                    <h5>1. ¿Qué es un sistema de información?<h5>
                 </div>
                 <div class="col col-lg-12">
                 <p>
-                 R// Todo comenzó a los finales de los años 50 cuando se creó ARPA (agencia de proyecto de investigación avanzada) en el seno del departamento de defensa de
-                 EEUU, Mas tarde en 1966 Lawrence G. Roberts se trasladó a ARPA para desarrollar el concepto de red de ordenadores, el ARPANET.
+                R// Es un conjunto de partes o elementos que interactúan con un objetivo común, pero de estos los que nos interesan son los sistemas.
                 </p>
                 </div>
             </div>
 
             <div class="row justify-content-md-center">
                 <div class="col col-lg-12">   
-                    <h5>2. ¿Cuál es el principal objetivo por el cual nació el internet?<h5>
-                </div>
-                <div class="col col-lg-12">
-                <p>
-                R// Es la comunicación: que permite al ser humano una comunicación sin barrera, que no tenga en cuenta el, espacio, ni frontera,
-                 distancia, sociedades.
-                </p>
-                </div>
-            </div>
-
-            <div class="row justify-content-md-center">
-                <div class="col col-lg-12">   
-                    <h5>3. Enumere las ventajas y desventajas al usar el internet.<h5>
-                </div>
-                <div class="col col-lg-12">
-                <p>
-                 R// Ventajas:
-                </p>
-                <ul>
-                    <li>1. Hacer la comunicación ms sencilla.</li>
-                    <li>2. Es posible conocer e interactuar con muchas personas todas partes del mundo.</li>
-                    <li>3. La búsqueda de información se vuelve mucho más sencilla, sin tener que ir forzadamente a las bibliotecas tradicionales.</li>
-                    <li>4. Es posible encontrar muchos puntos de vistas diferentes</li>
-                    <li>5. Sobre algunas noticias.</li>
-                    <li>6. Es posible la creación y descarga de software libre, por sus herramientas colaborativas.</li>
-                    <li>7. Es posible encontrar soportes técnicos de todas las clases sobre algunas herramientas o procesos.</li>
-                </ul>
-                 <p> Desventajas:</p>
-                 <ul>
-                    <li> 8. Así como es fácil encontrar información buena, es posible encontrar de la misma forma información mala, desagradable (pornografía, violencia explícita, terrorismo) afecta especialmente a los menores.</li>
-                    <li>     9. Te genera una gran dependencia o vicio de internet, descuidándote de muchas cosas personales o laborales.</li>
-                </ul>
-                </div>
-            </div>
-
-            <div class="row justify-content-md-center">
-                <div class="col col-lg-12">   
-                    <h5>   4. ¿Qué cantidad de usuario posee honduras al2015?<h5>
-                </div>
-                <div class="col col-lg-12">
-                <p>
-                 R// Posee 1.1 millones
-                </p>
-                </div>
-            </div>
-
-            <div class="row justify-content-md-center">
-                <div class="col col-lg-12">   
-                    <h5>5. ¿Enumere los servicios que ofrece los ISP?<h5>
-                </div>
-                <div class="col col-lg-12">
-                <p>
-                    R// Es la empresa que brinda conexión a internet a sus clientes,
-                    Se conecta a sus usuarios a internet a través de diferentes tecnologías
-                    Como ADSL, cable modem, GSM, dial-up etc.
-                </p>
-                </div>
-            </div>
-
-            <div class="row justify-content-md-center">
-                <div class="col col-lg-12">   
-                    <h5> 6. ¿Enumere los buscadores de internet más buscados?<h5>
-                </div>
-                <div class="col col-lg-12">
-                <p>
-                R// Son Google, Bing, Yahoo, Ask.com, Aol search, AltaVista, Cha-cha.
-                </p>
-                </div>
-            </div>
-                        <!--aaaaaaaaaaaaaaaaaaaaaaaaaaa-->
-            <div class="row justify-content-md-center">
-                <div class="col col-lg-12">   
-                    <h5>7. ¿Detalle los tipos de redes que existen?<h5>
+                    <h5>2. Enumere y explique cada uno de los tipos de sistemas de información<h5>
                 </div>
                 <div class="col col-lg-12">
                 <p>
                 R// Son:
                 </p>
                 <ul>
-                    <li>Red de área personal (PAM)</li>
-                    <li>Red de área local (LAN)</li>
-                    <li>Red de área de campus (CAN)</li>
-                    <li>Red de área metropolitana (MAN)</li>
-                    <li>Red de área amplia (WAN)</li>
-                    <li>Red de área de almacenamiento (SAN)</li>
-                    <li>Red de área local virtual (VLAN)</li>
+                    <li>Sistema de procesamiento de transacciones</li>
+                    <li>Sistema de control de procesos de negocio</li>
+                    <li>Sistema de colaboración empresarial</li>
+                    <li>Sistema de información de gestió</li>
+                    <li>Sistema de apoyo a la toma de decisiones</li>
+                    <li>Sistema de información ejecutiv</li>
                 </ul>
                 </div>
             </div>
 
             <div class="row justify-content-md-center">
                 <div class="col col-lg-12">   
-                    <h5>     8. ¿Cuáles son las topologías de red más conocidas?<h5>
+                    <h5>3. ¿Cuál es el ciclo de vida de un sistema de información?<h5>
                 </div>
                 <div class="col col-lg-12">
                 <p>
-                    R//Son árbol, estrella extendidas
+                R// es un conjunto de actividades que los analistas, diseñadores, desarrolladores, y usuarios realizan para desarrollar e implementar un sistema de 
+                información.
+                </p>
+                
+                </div>
+            </div>
+
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5>4. Enumere las ventajas que aportan a una empresa los sistemas de información.<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                 R// Ventaja:
+                </p>
+                <ul>
+                    <li>Correo electrónico personal o empresarial</li>
+                    <li>Comercio electrónico</li>
+                    <li>Chat en tiempo real</li>
+                    <li>Video llamada</li>
+                    <li>Transacciones bancarias</li>
+                    <li>Juegos y películas en líneas</li>
+                    <li>La biblioteca digital más grande del mundo</li>
+                </ul>
+                </div>
+            </div>
+
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5>5. ¿Cuáles son los componentes esenciales de un sistema información?<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                R// El hardware, equipo físico utilizado para procesar y almacenar datos, el hardware y los procesamientos utilizados para transformar y 
+                extraer información.
+                </p>
+                </div>
+            </div>
+
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5>6. ¿Cómo influye el ser humano el uso de sistemas de información?<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                R// Influye en el conocimiento en el uso de las computadoras.
+                </p>
+                </div>
+            </div>
+                        <!--aaaaaaaaaaaaaaaaaaaaaaaaaaa-->
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5>7. ¿Qué es piratería?<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                R// Es algo ilícito, fuera de la ley
+                </p>
+                </div>
+            </div>
+
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5>8. ¿Qué la ética computacional?<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                R// Es la transformadora de tres niveles distintos: Intervienen en el mundo modificando, sirve de elemento mediador en nuestras intervenciones 
+                en el modificador nuestras capacidades de acción y en capaz de crear novedosas estructuras de intervención.
                 </p>
                 </div>
             </div>
@@ -165,51 +159,60 @@
             
             <div class="row justify-content-md-center">
                 <div class="col col-lg-12">   
-                    <h5>9. ¿Qué es el navegador web?<h5>
+                    <h5>¿Qué es la piratería?<h5>
                 </div>
                 <div class="col col-lg-12">
                 <p>
-                R// Es la permite el acceso a la web, interpretando la información de distintitos tipos de archivos y sitios web para que estos puedan ser vistosa.
+                R// Es algo ilícito fue de la ley
                 </p>
                 </div>
             </div>
 
             <div class="row justify-content-md-center">
                 <div class="col col-lg-12">   
-                    <h5> 10. ¿Enumere los navegadores más buscados a nivel mundial?<h5>
+                    <h5> 10. ¿Cómo afecta a las empresas la violación de los derechos del autor?<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                R//Afecta obras de distintos tipos, como la música, la literatura etc.
+                </p>
+                </div>
+            </div>
+
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5>11. Enumere algunas responsabilidades que adquieren los profesionales información.<h5>
                 </div>
                 <div class="col col-lg-12">
                 <p>
                 R// Son:
-                <ol>
-                    <li>google Chrome 34.9%</li>
-                    <li>Internet Explorer 22.7%</li>
-                    <li>Firefox (Mozilla) 17.6% </li>
-                    <li>Safari </li>
-                </ol>
+                </p>
+                <ul>
+                    <li>Contar con un juicio sereno ético y moral</li>
+                    <li>Atacar y hacer cumplir las normas morales y ética</li>
+                    <li>Ser imparcial en los juicios que imite como auditor</li>
+                </ul>
+                </div>
+            </div>
+
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5>12. ¿De qué se encarga la seguridad informática?<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                R// De proteger la integridad y la privacidad de la información almacenada en un sistema informático.
                 </p>
                 </div>
             </div>
 
             <div class="row justify-content-md-center">
                 <div class="col col-lg-12">   
-                    <h5>  11. ¿Cuál es la diferencia entre un buscador y un navegador?<h5>
+                    <h5>13. ¿Cuál es la mejor manera de estar protegido al usar internet?<h5>
                 </div>
                 <div class="col col-lg-12">
                 <p>
-                R//Navegador: Es un programa que instala en ordenador para acceder a cualquier sitio web en internet, es decir para navegar en internet. 
-                Buscador: Es un sitio web al cual ingresa a través de un navegador y que te permite encontrar otros sitios web, imágenes o información sobre algo en internet.
-                </p>
-                </div>
-            </div>
-
-            <div class="row justify-content-md-center">
-                <div class="col col-lg-12">   
-                    <h5>12. ¿Qué es un buscador?<h5>
-                </div>
-                <div class="col col-lg-12">
-                <p>
-                R// Es donde se muestran varias direcciones de páginas que contienen temas que se están buscando.
+                R//Por en medio de un antivirus.
                 </p>
                 </div>
             </div>

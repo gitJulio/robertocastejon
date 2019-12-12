0a46bf3c6bf3dc0985e48ef70ed207979283d452
Julio Bueso <julio.bueso@allasrepuestos.com>
2019-12-12 08:10:46 -0600

    a

--- guia1.php
+++ guia1.php
@@ -1,93 +1,133 @@
-¿Explique la evolución del internet?
+<html>
+    <head>
+        <title>
+            Guia 1
+        </title>
+    </head>
+    <body>
+        
+        <h1>GUIA No. 1</h1>
+        <br>
+        
+        <div class="container">
+        <div class="row">
+            <div class="col-6">
+            One of three columns
+            </div>
+            <div class="col-3">
+            One of three columns
+            </div>
+            <div class="col-3">
+            One of three columns
+            </div>
+        </div>
+        </div>
 
-R// Todo comenzó a los finales de los años 50 cuando se creó ARPA (agencia de proyecto de investigación avanzada) en el seno del departamento de defensa de EEUU, Mas tarde en 1966 Lawrence G. Roberts se trasladó a ARPA para desarrollar el concepto de red de ordenadores, el ARPANET.
+        <h2>¿Explique la evolución del internet?<h2>
 
-2. ¿Cuál es el principal objetivo por el cual nació el internet?
+        R// Todo comenzó a los finales de los años 50 cuando se creó ARPA (agencia de proyecto de investigación avanzada) en el seno del departamento de defensa de EEUU, Mas tarde en 1966 Lawrence G. Roberts se trasladó a ARPA para desarrollar el concepto de red de ordenadores, el ARPANET.
 
-R// Es la comunicación: que permite al ser humano una comunicación sin barrera, que no tenga en cuenta el, espacio, ni frontera, distancia, sociedades.
 
-3. Enumere las ventajas y desventajas al usar el internet.
+        2. ¿Cuál es el principal objetivo por el cual nació el internet?
 
-R// Ventajas:
+        R// Es la comunicación: que permite al ser humano una comunicación sin barrera, que no tenga en cuenta el, espacio, ni frontera, distancia, sociedades.
 
-1. Hacer la comunicación ms sencilla.
 
-2. Es posible conocer e interactuar con muchas personas todas partes del mundo.
+        3. Enumere las ventajas y desventajas al usar el internet.
 
-3. La búsqueda de información se vuelve mucho más sencilla, sin tener que ir forzadamente a las bibliotecas tradicionales.
+        R// Ventajas:
 
-4. Es posible encontrar muchos puntos de vistas diferentes
+        1. Hacer la comunicación ms sencilla.
 
-5. Sobre algunas noticias.
+        2. Es posible conocer e interactuar con muchas personas todas partes del mundo.
 
-6. Es posible la creación y descarga de software libre, por sus herramientas colaborativas.
+        3. La búsqueda de información se vuelve mucho más sencilla, sin tener que ir forzadamente a las bibliotecas tradicionales.
 
-7. Es posible encontrar soportes técnicos de todas las clases sobre algunas herramientas o procesos.
+        4. Es posible encontrar muchos puntos de vistas diferentes
 
-Desventaja:
+        5. Sobre algunas noticias.
 
-8. Así como es fácil encontrar información buena, es posible encontrar de la misma forma información mala, desagradable (pornografía, violencia explícita, terrorismo) afecta especialmente a los menores.
+        6. Es posible la creación y descarga de software libre, por sus herramientas colaborativas.
 
-9. Te genera una gran dependencia o vicio de internet, descuidándote de muchas cosas personales o laborales.
+        7. Es posible encontrar soportes técnicos de todas las clases sobre algunas herramientas o procesos.
 
-4. ¿Qué cantidad de usuario posee honduras al2015?
+        Desventaja:
 
-R// Posee 1.1 millones
+        8. Así como es fácil encontrar información buena, es posible encontrar de la misma forma información mala, desagradable (pornografía, violencia explícita, terrorismo) afecta especialmente a los menores.
 
-5. ¿Enumere los servicios que ofrece los ISP?
+        9. Te genera una gran dependencia o vicio de internet, descuidándote de muchas cosas personales o laborales.
 
-R// Es la empresa que brinda conexión a internet a sus clientes,
 
-Se conecta a sus usuarios a internet a través de diferentes tecnologías
+        4. ¿Qué cantidad de usuario posee honduras al2015?
 
-Como ADSL, cable modem, GSM, dial-up etc.
+        R// Posee 1.1 millones
 
-6. ¿Enumere los buscadores de internet más buscados?
 
-R// Son Google, Bing, Yahoo, Ask.com, Aol search, AltaVista, Cha-cha.
+        5. ¿Enumere los servicios que ofrece los ISP?
 
-7. ¿Detalle los tipos de redes que existen?
+        R// Es la empresa que brinda conexión a internet a sus clientes,
 
-R// Son:
+        Se conecta a sus usuarios a internet a través de diferentes tecnologías
 
-· Red de área personal (PAM)
+        Como ADSL, cable modem, GSM, dial-up etc.
 
-· Red de área local (LAN)
 
-· Red de área de campus (CAN)
+        6. ¿Enumere los buscadores de internet más buscados?
 
-· Red de área metropolitana (MAN)
+        R// Son Google, Bing, Yahoo, Ask.com, Aol search, AltaVista, Cha-cha.
 
-· Red de área amplia (WAN)
 
-· Red de área de almacenamiento (SAN)
+        7. ¿Detalle los tipos de redes que existen?
 
-· Red de área local virtual (VLAN)
+        R// Son:
 
-8. ¿Cuáles son las topologías de red más conocidas?
+        · Red de área personal (PAM)
 
-R//Son árbol, estrella extendidas
+        · Red de área local (LAN)
 
-9. ¿Qué es el navegador web?
+        · Red de área de campus (CAN)
 
-R// Es la permite el acceso a la web, interpretando la información de distintitos tipos de archivos y sitios web para que estos puedan ser vistosa.
+        · Red de área metropolitana (MAN)
 
-10. ¿Enumere los navegadores más buscados a nivel mundial?
+        · Red de área amplia (WAN)
 
-R// Son google Chrome 34.9%
+        · Red de área de almacenamiento (SAN)
 
-Internet Explorer 22.7%
+        · Red de área local virtual (VLAN)
 
-Firefox (Mozilla) 17.6%
 
-Safari
+        8. ¿Cuáles son las topologías de red más conocidas?
 
-11. ¿Cuál es la diferencia entre un buscador y un navegador?
+        R//Son árbol, estrella extendidas
 
-R//Navegador: Es un programa que instala en ordenador para acceder a cualquier sitio web en internet, es decir para navegar en internet.
 
-Buscador: Es un sitio web al cual ingresa a través de un navegador y que te permite encontrar otros sitios web, imágenes o información sobre algo en internet.
+        9. ¿Qué es el navegador web?
 
-12. ¿Qué es un buscador?
+        R// Es la permite el acceso a la web, interpretando la información de distintitos tipos de archivos y sitios web para que estos puedan ser vistosa.
 
-R// Es donde se muestran varias direcciones de páginas que contienen temas que se están buscando.
+
+        10. ¿Enumere los navegadores más buscados a nivel mundial?
+
+        R// Son google Chrome 34.9%
+
+        Internet Explorer 22.7%
+
+        Firefox (Mozilla) 17.6%
+
+        Safari
+
+
+        11. ¿Cuál es la diferencia entre un buscador y un navegador?
+
+        R//Navegador: Es un programa que instala en ordenador para acceder a cualquier sitio web en internet, es decir para navegar en internet.
+
+        Buscador: Es un sitio web al cual ingresa a través de un navegador y que te permite encontrar otros sitios web, imágenes o información sobre algo en internet.
+
+
+        12. ¿Qué es un buscador?
+
+        R// Es donde se muestran varias direcciones de páginas que contienen temas que se están buscando.
+
+    </body>
+</html>
+

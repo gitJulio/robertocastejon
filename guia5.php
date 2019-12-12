546f76d509e57c0ec59ec5eca3bb43488870bc4c
Julio Bueso <julio.bueso@allasrepuestos.com>
2019-12-12 10:36:13 -0600

    a

---
+++ guia5.php
@@ -0,0 +1,220 @@
+<html>
+    <head>
+        <title>
+            Guia 1
+        </title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+<link href="css/heroic-features.css" rel="stylesheet">
+    </head>
+    <body>
+          <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">UCENM</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php">Inicio
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <!-- <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contacto</a>
+          </li> -->
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+        <br>
+
+
+          <center>
+          <h1>GUIA No. 5</h1>
+          </center>
+               
+
+
+        
+        <div class="container">
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5>1. ¿Qué es una base de datos?<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                R// Es un almacén donde se3 guardamos una colección o conjunto de información (textos, imágenes, sonidos, videos juegos) las cuales se encuentran relacionadas entre si 
+                y pueden ser accesibles y consultadas en cualquier momento.
+                </p>
+                </div>
+            </div>
+
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5>2. Enumere la clasificación de las bases de datos.<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                <ul>
+                    <li>
+                        <p>Según la variable de la base de datos</p>
+                        <ol>
+                            <li>Base de datos estáticas</li>
+                            <li>Base de datos dinámicas</li>
+                        </ol>
+                    </li>
+                </ul>
+                </div>
+            </div>
+
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5>3. ¿Qué tipos de datos se pueden manejar en Excel?<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                R// Textos, valores numéricos y formulas
+                </p>
+                
+                </div>
+            </div>
+
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5>4. ¿Cuáles son los pasos para mover o copiar rangos de textos? Puede indicar cualquiera de los medios.<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                R// 1 Seleccionar el rango que dese mover. 
+                2 Llamar al menú contextual (Clic derecho) cortar o clic en el botón cortar o presionar
+                 la tecla ctrl. tx observe que al rededor del rango seleccionado aparece un borde móvil punteado. Esto indica que ese rango va a ser movido.
+                 3 Seleccionar una celda destino donde se desea que el rango sea movido. 4 Presionar entre
+                </p>
+                </div>
+            </div>
+
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5>5. ¿Cuándo se debe usar funciones avanzadas en Excel?<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                R// Cuando se necesite de cualquier cálculo determinado de cualquier índole, pues se escribe la respectiva función seguida de sus argumentos.
+                </p>
+                </div>
+            </div>
+
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5>6. Desarrolle un ejercicio por cada una de las funciones: Suma, Max, Min, promedio<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                    <ul>
+                        <li>=Suma (B4: B11)</li>
+                        <li>= Max (B4: B11)</li>
+                        <li>= Min (B4: B11)</li>
+                        <li>= PROMEDIO (B4: B11)</li>
+                    </ul>
+                </p>
+                </div>
+            </div>
+                        <!--aaaaaaaaaaaaaaaaaaaaaaaaaaa-->
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5>7. ¿Qué es ordenamiento en Excel?<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                R// Es ordenar datos de un rango o tabla
+                </p>
+                
+                </div>
+            </div>
+
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5>8. ¿Parar que sirve la función subtotales?<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                R// Nos ayuda a calcular el subtotal de una lista de valores.
+                </p>
+                </div>
+            </div>
+
+            
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5>9. ¿Qué es una tabla dinámica en Excel?<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                R// Es un tipo de tabla que nos permite decidir con facilidad los campos que aparecen como columnas, como filas y 
+                como valores de la tabla y podemos hacer modificaciones a dicha definición en el momento que lo deseamos.
+                </p>
+                </div>
+            </div>
+
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5> 10. ¿Cuál es la diferencia entre un gráfico dinámico y uno convencional?<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                R//Grafico dinámico: es el que está basado en los datos de una tabla dinámica y se ajusta automáticamente al aplicar cualquier filtro sobre la información.
+                Grafico convención: es el que posee un panel de filtros
+                </p>
+                </div>
+            </div>
+
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5>11. ¿Para qué sirve la validación en Excel?<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                R// Es un servicio que provee a los usuarios de internet un sistema para poder almacenar información, imágenes, videos, o cualquier contenido accesible vía web.
+                </p>
+                </div>
+            </div>
+
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5>12. ¿Para qué sirven los macros en Excel?<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                R// Es la que nos ayuda a automatizar aquellas tareas que hacemos repetidamente. Una macro es una serie de instituciones que son
+                 guardadas dentro de un archivo en excel para poder ser ejecutados cuando lo necesitamos.
+                </p>
+                </div>
+            </div>
+
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <h5>13. ¿Cuál es el método abreviado para ejecutar una macro en Excel?<h5>
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                R// Es la permite ejecutar la macro en cualquier momento es ctrl +tecla.
+                </p>
+                </div>
+            </div>
+
+
+        </div>        
+
+    </body>
+</html>
+
